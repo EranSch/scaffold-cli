4540e28148e00828898499ed76bd04ff7f12ee2a
Eran Schoellhorn <eschoellhorn@352inc.com>
2017-10-26 06:18:29 -0400

    Add inline comments and tune whitespace

--- commands/create.js
+++ commands/create.js
@@ -6,15 +6,15 @@ const handlers = require('../handlers');
 const prompt = require('../helpers/prompt');
 
 module.exports = async (name, type) => {
-
+  // Prompt for name if not provided
   if (!name) name = await prompt('What is the name of your project?');
 
+  // Prompt for project type if not provided or invalid, allow selection from supported handlers
   if (!type) type = await prompt('Which type of project would you like to create?', keys(handlers));
-
   if (!handlers[type]) type = await prompt('Invalid project type specific, please select from list below:', keys(handlers));
 
+  // Exit out if git not found
   if (!shell.which('git')) return console.log(colors.red('you must have git installed'));
 
   handlers[type](name);
-
 };
